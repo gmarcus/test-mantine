36523116bd4b4f2a0857fa1fe25e77e14d297e33
Glenn Marcus <glenn@cliqconsulting.com>
2024-01-15 01:47:36 +0000

    layout test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import { Box, Button, Container, SegmentedControl } from "@mantine/core"
+import { ImageActionBanner } from "../components/ImageActionBanner"
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return <>
+    <Box bg={"red"}>
+      <Container size="xl" maw={1440} bg={"gray"}>
+        <h1>Home page</h1>
+        <p>Hi Glenn</p>
+        <Button variant="filled">Button</Button>
+        <Box>
+          <SegmentedControl w={"400"} data={['Learners', 'Instructors']} />
+        </Box>
+        <Container size={"xs"} mt={60}>
+          <ImageActionBanner />
+        </Container>
+      </Container>    
+    </Box>
+  </>
 }
